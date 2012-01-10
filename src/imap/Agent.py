a6707c5c0e990c713a1711a51546b4f992846ba6
Kerem Eryılmaz <Kerem@144.122.98.139>
2012-01-10 16:14:12 +0200

    Some progress, whatever

--- src/imap/Agent.py
+++ src/imap/Agent.py
@@ -3,12 +3,13 @@ Created on 10 Oca 2012
 
 @author: Kerem
 '''
+from midiutil.MidiFile import MIDIFile
 
 class Agent(object):
     '''
     classdocs
     '''
-
+    performance = MIDIFile()
 
     def __init__(selfparams):
         '''

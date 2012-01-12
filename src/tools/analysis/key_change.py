56cf64f7e083713d053dc5e3221bbe7c02706ca8
Kerem <Kerem@Kerem-PC>
2012-01-12 15:16:14 +0200

    OPEN - task 3: Implement Key Change Function http://github.com/keryil/Expressive-Music-Performance-Using-Imitative-Agents/issues/issue/3
    Finished implementing key change detection.
    MIDI change

--- src/tools/analysis/key_change.py
+++ src/tools/analysis/key_change.py
@@ -9,12 +9,14 @@ There are 960 beats per measure in the midi
 import math
 from tools import log
 
-
+logger = None
 CMaj = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
 CMin = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
 notes = ("C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B")
 rotate_array = lambda x,t: x[-t:] + x[:-t]
 midi_to_note = lambda x: notes[x % 12]
+notes_in_measure =  lambda n, s, e: [note for note in n if (note.time >= s) and (note.time < e)]
+durations_to_array = lambda d: [d[note] for note in notes]
 
 # 24 12-dimensional vectors, one for each major/minor key
 key_vectors = {"C Major": CMaj, "C Minor": CMin}
@@ -37,14 +39,68 @@ def analyze_key_change(midi):
     track = midi.tracks[0]
     noteList = [noteEvent for noteEvent in track.eventList if noteEvent.type == "note"]
     keys = []
+    keyChange = [0. for i in range(6)]
     
     # process measure by measure
-    for i in range(0,7*960,960):
-        track.
-        
-def determine_key(notes):
-    pass
+    for i in range(0,6*960,960):
+        # first measure
+        if i == 0:
+            continue
+#            relevantNotes = notes_in_measure(noteList, i, i+960)
+#            keys.append(determine_key(relevantNotes))
+        #second measure
+        elif i == 960:
+            referenceNotes = notes_in_measure(noteList, i-960,i)
+            referenceKey, referenceCorr = determine_key(referenceNotes, i)
+            relevantNotes = notes_in_measure(noteList, i, i+960)
+            newCorr = correlate(key_vectors[referenceKey],durations_to_array(get_durations(relevantNotes, i+960)))
+            keyChange[i/960] = newCorr - referenceCorr
+        else:
+            referenceNotes = notes_in_measure(noteList, i-2*960,i)
+            referenceKey, referenceCorr = determine_key(referenceNotes, i)
+            relevantNotes = notes_in_measure(noteList, i, i+960)
+            newCorr = correlate(key_vectors[referenceKey],durations_to_array(get_durations(relevantNotes, i+960)))
+            keyChange[i/960] = newCorr - referenceCorr
+        print "Key change: ", keyChange
+    return keyChange
 
+def notes_in_measure(noteList, start, end):
+    in_measure = []
+    for note in noteList:
+        if (note.time >= start) and (note.time < end):
+            in_measure.append(note)                
+        # we also need notes that started the previous measure and lingers into this one
+        if (note.time < start) and (note.time + note.duration > start):
+            in_measure.append(note)
+    return in_measure
+        
+def determine_key(noteList, end):
+    #    for note in noteList:
+#        # if a note exceeds the measure boundary, just take the relevant part
+#        duration = min(note.duration, end - note.time)
+#        total_duration += duration
+#        durations[midi_to_note(note.pitch)] += duration
+#    durations = {note:dur/total_duration for note, dur in durations.items()}
+    durations = get_durations(noteList, end)    
+    max_correlation = -1.
+    best_key = None
+    for key, refVector in key_vectors.items():
+        corr = correlate(refVector, durations_to_array(durations))
+        if corr > max_correlation:
+            max_correlation = corr
+            best_key = key
+    return best_key, max_correlation
+        
+def get_durations(noteList, end):
+    durations = {note: 0. for note in notes}
+    for note in noteList:
+        # if a note exceeds the measure boundary, just take the relevant part
+        duration = min(note.duration, end - note.time)
+        durations[midi_to_note(note.pitch)] += duration
+    total_duration = sum(durations.values())
+    print durations, end
+    durations = {note:(dur/total_duration) for note, dur in durations.items()}
+    return durations
 
 def correlate(vector1, vector2):
     vector1_average = sum(vector1)/12.
@@ -64,9 +120,16 @@ if __name__ == "__main__":
 #    print CMaj
 #    print rotate_array(CMaj,1)
 #    print rotate_array(CMaj,2)
-    print midi_to_note(59)
-    print midi_to_note(60)
-    print midi_to_note(70)
+    from tools import midi as mid
+    midi = mid.prepare_initial_midi("../../../res/midi_text.txt", "../../../res/sample.midi", 15200)
+    track = midi.tracks[0]
+    noteList = [noteEvent for noteEvent in track.eventList if noteEvent.type == "note"]
+#    for i in range(0,6*960,960):
+#        in_measure = notes_in_measure(noteList, i, i + 960)
+#        print [midi_to_note(note.pitch) for note in in_measure]
+#        print determine_key(in_measure, i+960)
+    print analyze_key_change(midi)
+        
 #    for k in sorted(key_vectors.keys()):
 #        print "%012s: %s" % (k,key_vectors[k])
     

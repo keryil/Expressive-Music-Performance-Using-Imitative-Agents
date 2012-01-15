27fde5f3c011b4b0e4443c37a54c8d49787993ca
Kerem <Kerem@Kerem-PC>
2012-01-15 16:51:47 +0200

    OPEN - task 8: Implement Rule Functions http://github.com/keryil/Expressive-Music-Performance-Using-Imitative-Agents/issues/issue/8^
    Added initial evaluation functions to the agents. 
    Started to implement the rule functions.

--- src/imap/agent.py
+++ src/imap/agent.py
@@ -5,30 +5,56 @@ Created on 10 Oca 2012
 '''
 from midiutil.MidiFile import MIDIFile
 from tools import log
+from tools.analysis import rules
 
 class Agent(object):
     '''
     classdocs
     '''
     performance = None
-    id = None
+    __id = None
+    __weight_tempo = None
+    __weight_loudness = None
+    __weight_tempo_rules = None
+    __weight_loudness_rules = None
     __logger = None
-    def __init__(self, id):
+    
+    def __init__(self, id, weight_tempo, weight_loudness, weight_tempo_rules, weight_loudness_rules):
         '''
         Constructor
         '''
-        self.id = id
+        # check if tempo and loudness weights sum up to 1
+        assert 1 - (weight_loudness + weight_tempo) < 0.001
+        
+        self.__id = id
         self.__logger = log.get_agent_logger(__name__, id)
+        self.__weight_loudness = weight_loudness
+        self.__weight_loudness_rules = weight_loudness_rules
+        self.__weight_tempo = weight_tempo
+        self.__weight_tempo_rules = weight_tempo_rules
 #        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level="INFO")
         
+    def getId(self):
+        return self.__id
+    
     def perform(self):
         self.__logger.debug("Performing")
     
     def listen(self, performance):
         self.__logger.debug("Listening")
     
-    def evaluate(self, performance):
+    
+    def __evaluate_loudness(self,performance):
+        pass
+
+
+    def __evaluate_tempo(self,performance):
+        pass
+
+
+    def evaluate(self,performance):
         self.__logger.debug("Evaluating")
+        return self.__evaluate_tempo(performance) * self.__weight_tempo + self.__evaluate_loudness(performance) * (1-self.__weight_loudness)
     
 #    def __debug(self, message):
 #        self.__logger.debug("Agent %s |::| %s::%s" % (self.id, inspect.stack()[1][3], message))
---
+++ src/tools/analysis/rules.py
@@ -0,0 +1,26 @@
+'''
+Created on 15 Oca 2012
+
+@author: Kerem
+'''
+
+
+def rule1(group_structure):
+    pass
+def __rule1_tempo(group_structure):
+    pass
+def __rule1_loudness(group_structure):
+    pass
+
+def rule2():
+    pass
+def rule3():
+    pass
+def rule4():
+    pass
+def rule5():
+    pass
+    
+
+if __name__ == '__main__':
+    pass
